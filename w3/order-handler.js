e1e5b8abd57effd3bab98b94eae4dc25740e38fd
Miguel Reynaga <miguelreynaga34@gmail.com>
2026-01-30 01:04:20 -0800

    Step 3 Complete

--- w3/order-handler.js
+++ w3/order-handler.js
@@ -1,36 +1,40 @@
-const orderform = document.getElementById('orderform'); 
+const orderForm = document.getElementById('orderForm'); 
 
-const qtyInput = orderform.querySelector('#qty'); 
+const qtyInput = orderForm.querySelector('#qty');  
 
-const giftwrapInput = orderform.querySelector('#giftwrap'); 
+const giftwrapInput = orderForm.querySelector('#giftwrap'); 
 
-const sizeRadio = orderform.querySelectorAll('input[name="size"]');
+const sizeRadio = orderForm.querySelectorAll('input[name="size"]');
 
 
 const getSelectedRadioValue = function(radioButtons){
+        
+    
     for(const radio of radioButtons) {
         if (radio.checked) {
+           
             console.log(`${radio.value} has the attribute of ${radio.checked}`);
             return radio.value;
         }
     }
-    }
+    };
+
+
+
 
 
     export const getOrderInputs = function (){
-        console.log('get order from inputs'); 
-
+        console.log('get Order Inputs'); 
+       
 
 
 
         return{ 
             qty: parseInt(qtyInput.value) || 1, 
-            size: getSelectedRadioValue(sizeRadio), 
-            giftwrap: getOrderInputs.checked
+            size: getSelectedRadioValue(sizeRadio) || "S", 
+            giftwrap: giftwrapInput.checked
         }
 
 
     }
 
-    // TEMPORARY TEST:
-console.log(getOrderInputs()); 
