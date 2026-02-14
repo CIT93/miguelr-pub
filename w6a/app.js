7f22246c969632e8c21b33c5975e30905d218a8f
Miguel Reynaga <miguelreynaga34@gmail.com>
2026-02-14 15:37:27 -0800

    Step 2 Complete

--- w6a/app.js
+++ w6a/app.js
@@ -1,6 +1,7 @@
 console.log('Hello from app.js! Your JavaScript is connected and running!');
 import * as orderHandler from './order-handler.js'; 
 import * as calculator from './price-calculator.js'; 
+import * as resultsDisplay from './results-display.js' 
 const orderForm = document.getElementById('orderForm');
 
 const messageDisplayElement = document.getElementById ('ordersummary');
@@ -12,31 +13,26 @@ const handleOrderSubmit = function(event){
     event.preventDefault();
     const formData = orderHandler.getOrderInputs();
     
-        console.log(`Order Inputs - Object Literal:`);
-        console.log(`key of qty value of  ${formData.qty}`);
-        console.log(`key of giftwrap value of  ${formData.giftwrap}`);
-        console.log(`key of size value of  ${formData.size}`);
-        console.log(formData)
+       
 
         const calculatedPrice = calculator.calculateTotal(formData.qty, formData.giftwrap); 
 
         
-        const newEntry = {
+        const newOrder = {
             ...formData, 
             total:calculatedPrice, 
+            giftwrap:formData.giftwrap, 
             timeStamp: new Date().toISOString(),
            
          }
-            orders.push(newEntry); 
+            orders.push(newOrder); 
 
-            console.log("Orders array:",orders); 
-
-            messageDisplayElement.textContent = `Ordered ${formData.qty} ${formData.size} T-Shirts. Total: $${calculatedPrice}`;
+           resultsDisplay.displayOrder(newOrder)
+        
     }; 
     
 
-
-
+   
 
 
 const init = function(){
